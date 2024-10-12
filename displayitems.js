c81167405dc97bb83f8af197ca37eb7c754ed2d7
MarionRosete <marionrosete1@gmail.com>
2024-10-12 18:53:20 +0800

    update lists

---
+++ displayitems.js
@@ -0,0 +1,32 @@
+import items from './items.js'; // Import the items array
+
+function displayItems() {
+    console.log('items', items);
+    
+    const container = document.getElementById('item-container');
+
+    // Loop through each item in the array
+    items.forEach(item => {
+        // Create a div for each item
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'grid-item';
+
+        // Add content to the div, including the image
+        itemDiv.innerHTML = `
+            <div class="shoe-item">
+          <img src="${item.image}" alt="Dc Shoes Teknic Off White" class="responsive-image"></img>
+
+                <div class="content">
+                    <p>${item.name}</p>
+                    <p>Size: ${item.size}</p>
+                </div>
+            </div>
+        `;
+
+        // Append the div to the container
+        container.appendChild(itemDiv);
+    });
+}
+
+
+displayItems()
---
+++ items.js
@@ -0,0 +1,101 @@
+const items = [
+    { 
+        image: "./public/og2012.webp", 
+        name: 'Nike sb stefan janoski OG Venom 2012',
+        size: '10.5us'
+    },
+   { 
+       image: "./public/og2018.webp", 
+        name: 'Nike sb stefan janoski OG Venom 2018',
+        size: '10.5us'
+    },
+    { 
+        image: "./public/flamingo.webp", 
+        name: 'Nike sb stefan janoski Vast Gray',
+        size: '10.5us'
+    },
+    { 
+       image: "./public/slip+ summit.png", 
+        name: 'Nike sb stefan janoski Flamingo ',
+        size: '10.5us'
+    },
+   { 
+       image: "./public/blackfloral.webp", 
+        name: 'Nike sb stefan janoski Black Floral',
+        size: '10us'
+    },
+
+     { 
+       image: "./public/woodgrain.webp", 
+        name: 'Nike sb stefan janoski Wood Grain',
+        size: '10us'
+    },
+    { 
+        image: "./public/rmcanvas.webp", 
+        name: 'Nike sb stefan janoski RM Canvas',
+        size: '10us'
+    },
+     { 
+       image: "./public/urbancamo.webp", 
+        name: 'Nike sb stefan janoski Urban Camo',
+        size: '9.5us'
+    },
+    { 
+       image: "./public/britishtan.webp", 
+        name: 'Nike sb stefan janoski British Tan',
+        size: '8.5us'
+    },
+     { 
+       image: "./public/slip+black.webp", 
+        name: 'Nike sb stefan janoski Slip+ Venom',
+        size: '8.5us'
+    },
+     { 
+       image: "./public/aztecblack.webp", 
+        name: 'Nike sb stefan janoski RM Black Aztec',
+        size: '8us'
+    },
+     { 
+       image: "./public/phantomblue.webp", 
+        name: 'Nike sb stefan janoski Obsidian Phantom Blue',
+        size: '8us'
+    },
+    { 
+       image: "./public/midnight.webp", 
+        name: 'Nike sb stefan janoski Midnight Green',
+         size: '8us'
+    },
+    { 
+       image: "./public/irongray.webp", 
+        name: 'Nike sb stefan janoski RM Iron Gray',
+        size: '7.5us'
+    },
+    { 
+       image: "./public/chron2.webp", 
+        name: 'Nike sb Chron 2',
+        size: '10.5us'
+    },
+    { 
+       image: "./public/force58.webp", 
+        name: 'Nike sb Force 58',
+        size: '9us'
+    },
+    { 
+       image: "./public/af1.webp", 
+        name: 'Nike Air Force 1 FlyEase',
+         size: '10.5us'
+    },
+    { 
+       image: "./public/vansslip.webp", 
+        name: 'Vans Classic Slip',
+         size: '8us, 10us'
+    },
+    { 
+       image: "./public/dcslip.jpg", 
+        name: 'DC Suede slip',
+         size: '10.5us'
+    },
+   
+];
+
+export default items; 
