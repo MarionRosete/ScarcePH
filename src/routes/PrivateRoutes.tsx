9fb984ad28ab593ef952d1b4fde00983a18cb205
M4rxDev <marionrosete1@gmail.com>
2026-01-23 02:15:46 +0800

    cold start msg

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,15 +1,28 @@
 import { Navigate, Outlet, useLocation } from 'react-router';
 import { useAuth } from '@/context/AuthContext';
 import LoadingScreen from '@/pages/LoadingScreen';
+import { useEffect, useState } from 'react';
 
 
 export const PrivateRoute: React.FC = () => {
   const { isAuthenticated, isLoading } = useAuth();
   const location = useLocation();
 
+  const [delay, setDelay] = useState(false)
 
+  useEffect(() => {
+    const timer = setTimeout(() => setDelay(true), 3000)
+    return () => clearTimeout(timer)
+  }, [])
+  
   if (isLoading) {
-    return <LoadingScreen msg={"Loading authentication status… "}/>
+    return <LoadingScreen 
+      msg={
+        delay
+          ? "Server is waking up (free tier). Almost there…"
+          : "Checking authentication…"
+      } 
+    />
   }
 
  
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,11 +1,25 @@
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "@/context/AuthContext";
 import LoadingScreen from "@/pages/LoadingScreen";
+import { useEffect, useState } from "react";
 
 export const PublicRoute = () => {
   const { isAuthenticated, isLoading } = useAuth();
 
-  if (isLoading) return  <LoadingScreen msg={"Loading authentication status… "}/>
+    const [delay, setDelay] = useState(false)
+  
+    useEffect(() => {
+      const timer = setTimeout(() => setDelay(true), 3000)
+      return () => clearTimeout(timer)
+    }, [])
+
+  if (isLoading) return  <LoadingScreen 
+      msg={
+        delay
+          ? "Server is waking up (free tier). Almost there…"
+          : "Checking authentication…"
+      } 
+    />
   return isAuthenticated
     ? <Navigate to="/dashboard" replace />
     : <Outlet />;
